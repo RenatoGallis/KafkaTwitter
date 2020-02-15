47e40087c28190b1186d541a1e34edfb7ebfe755
Renato <gallis.renato@gmail.com>
2020-02-15 14:34:51 -0300

    Iniciando projeto kafka-HDFS

---
+++ src/main/java/TwitterConsumer.java
@@ -0,0 +1,182 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwitterConsumer {
+
+	public static void main(String[] args) {
+		new TwitterConsumer().run();// Acesso o mmetodo da classe pelo construtor que instanciei
+
+	}
+
+	// construtor da classe ConsumerDemoThread para inicializa-la
+	private TwitterConsumer() {
+	}
+
+	private void run() {
+
+		Logger logger = LoggerFactory.getLogger(TwitterConsumer.class.getName());// logar o que desejar da classe
+		String bootstrapserver = "127.0.0.1:9092";
+		String groupID = "my-seventh-application";
+		String auto_offset_reset_config = "latest";//latest - earliest 
+		// variavel do topico:
+		String topic = "twitter_topic";
+		// Trava para para o consumer
+		CountDownLatch trava = new CountDownLatch(1);
+
+		// Criando o consumidor com a thread
+		logger.info("Criando minha thread de consumer");
+//		Runnable myConsumerThread = new ConsumerThreads(trava, topic, bootstrapserver, groupID,
+//				auto_offset_reset_config);
+
+		Runnable myConsumerThread = new ConsumerThreads(trava, topic, bootstrapserver, groupID,
+				auto_offset_reset_config);
+
+		// começa o fluxo da thread
+		Thread myThread = new Thread(myConsumerThread);
+		myThread.start();// começa com a thread
+
+		// Começo o tratamento para fechar a aplicação corretamente
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			logger.info("Desligando o consumer");
+			((ConsumerThreads) myConsumerThread).shutdown();
+			try {
+				trava.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				logger.error("Aplicação interrompida");
+			}
+			logger.info("Aplicação terminada");
+
+		}));
+
+		try {
+			trava.await();
+		} catch (InterruptedException e) {
+			logger.error("Aplicação interrompida" + e);
+		} finally {
+			//
+			logger.info("forçando o final do rolê");
+		}
+	}
+
+	// Classe para disparar uma thread precisa implementar Runnable
+	public class ConsumerThreads implements Runnable {
+
+		private Logger logger = LoggerFactory.getLogger(ConsumerThreads.class.getName());// logar o que desejar da
+																							// classe
+		private CountDownLatch trava;// para fazer a contagem da trava para parar o consumo
+		private KafkaConsumer<String, String> consumer;
+
+		public ConsumerThreads(CountDownLatch trava, String topic, String bootstrapserver, String groupID,
+				String auto_offset_reset_config) {
+
+			this.trava = trava;
+			// Criar as propriedades do consumer
+			Properties properties = new Properties();
+			properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapserver);// servidor do broker kafka
+																								// onde vai se conectar
+			properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());// deserializando
+																														// chave
+																														// tem
+																														// que
+																														// ser
+																														// o
+																														// mesmo
+																														// formato
+																														// que
+																														// o
+																														// produtor
+			properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());// deserializando
+																														// valor
+																														// tem
+																														// que
+																														// ser
+																														// o
+																														// mesmo
+																														// formato
+																														// que
+																														// no
+																														// produtor
+			properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID); // ID do grupo de consumidores que esse
+																				// consumer faz parte (boa pratica todo
+																				// consumer precisa pertencer a um
+																				// consumer-group)
+			properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, auto_offset_reset_config); // Define onde
+																										// meu offset
+																										// irá ler as
+																										// mensagens
+																										// (mais cedo
+																										// mais antigo
+																										// pulando
+																										// alguns
+																										// offsets etc)
+			// Aqui termina a configuração do consumidor
+
+			consumer = new KafkaConsumer<String, String>(properties);// Criando o consumidor com o tipo de dado String e
+																		// passando as propridades acima como
+																		// configuração
+			consumer.subscribe(Arrays.asList(topic));// o metodo Arrays.asList da a capacidade de consumir de varios
+														// tópicos distintos
+		}
+
+		public void run() {
+			// poll data temos que lançar uma exceção
+			try {
+				List<String> lines = null;
+			    BufferedWriter buffWriter = null;
+		    	Path file = Paths.get("C:\\Users\\Renato Gallis\\Desktop\\twitter.txt");
+				
+				while (true) {
+					// Criando um consumer records para pegar a mensagem que esta sendo pesquisada
+					// de 100 mils em 100 mils
+					ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+
+					for (ConsumerRecord<String, String> record : records) {
+						// Gerar arquivo e mandar para o HDFS nesse ponto *** Renato Gallis****
+//						logger.info("Key:" + record.key() + "\n" + "Value:" + record.value() + "\n" + "Partition:"
+//								+ record.partition() + "\n" + "Offset:" + record.offset() + "TimeStamp:" + record.timestamp());
+//						lines = Arrays.asList(record.key(),record.value());
+						 buffWriter.write(record.value() + System.lineSeparator());
+				            buffWriter.flush();
+					}
+//					Files.write(file,lines,StandardOpenOption.APPEND);
+					
+				}
+			
+			} catch (WakeupException exception) {
+				logger.info("As mensagens foram recebidas!");
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				// fechar o consumidor depois de ler a mensagens
+				consumer.close();
+				// fale para o main() que fechei o consumidor
+				trava.countDown();
+			}
+		}
+
+		public void shutdown() {
+			// interromper o consumer.poll(); que é a pesquisa por mensagens
+			consumer.wakeup();
+
+		}
+	}
+
+}
---
+++ src/main/java/TwitterProducer.java
@@ -0,0 +1,153 @@
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.Hosts;
+import com.twitter.hbc.core.HttpHosts;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+public class TwitterProducer {
+
+	public TwitterProducer() {}
+	//criando rastreio de loger na classe
+		Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
+		String consumerKey="dqXM1Ch9CAUwsi6J5ltJ4QPK4";
+		String consumerSecret="4eHvb7iTPInDmtQxfGBMLfiP7tjqjWNFoxbQOIuErUj1E3zQro";
+		String token="792793562064748544-DlNskS8nobKVRir5k7HIdgfJKc7yeUt";
+		String secret="J0fx96ge9hyLZQI3xaGmkKu3K3If5GxbxkVrFSlfNOZQd";
+		
+		List<String> terms = Lists.newArrayList("Canada","Toronto","Montreal","Quebec");
+		
+	public static void main(String[] args) {
+	
+	new TwitterProducer().run();
+		
+	}
+	
+	public void run() {
+	   
+		logger.info("Inicio");
+		BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(1000);
+		//criar um twitter client
+		Client client = createTwitterClient(msgQueue);
+		// Attempts to establish a connection.
+		client.connect();
+		//criar um kafka producer
+		KafkaProducer<String, String> producer = createKafkaProducer();
+		
+		//Adicionando shutdownhook
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			
+			logger.info("Parando aplicação...");
+			logger.info("Parando conexão com o Twitter...");
+			client.stop();
+			logger.info("Fechando producer...");
+			producer.close();
+		} ));
+		//loop para mandar mensagens para o kafka
+		
+		while (!client.isDone()) {
+			  String msg = null; 
+			  try {
+			  msg = msgQueue.poll(5,TimeUnit.SECONDS);
+			  }catch(InterruptedException e) {
+				  e.printStackTrace();
+				  client.stop();
+			  }
+			  if(msg!= null) {
+				  logger.info(msg);
+				  producer.send(new ProducerRecord<String, String>("twitter_topic",null, msg),new Callback() {
+					
+					@Override
+					public void onCompletion(RecordMetadata metadata, Exception e) {
+						if(e!= null) {
+							logger.error("Something bad happened",e);
+						}
+						
+					}
+				});
+			  }
+			  
+			}
+		logger.info("End of application");
+	}
+	
+
+   //Criando o client para o twitter
+	public Client createTwitterClient(BlockingQueue<String> msgQueue) {
+		
+		Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
+		StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
+		//List<Long> followings = Lists.newArrayList(1234L, 566788L);
+		
+		//hosebirdEndpoint.followings(followings);
+		hosebirdEndpoint.trackTerms(terms);
+
+		// These secrets should be read from a config file
+		Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
+		
+		ClientBuilder builder = new ClientBuilder()
+				  .name("Hosebird-Client-01")      // optional: mainly for the logs
+				  .hosts(hosebirdHosts)
+				  .authentication(hosebirdAuth)
+				  .endpoint(hosebirdEndpoint)
+				  .processor(new StringDelimitedProcessor(msgQueue));
+				//  .eventMessageQueue(eventQueue);    // optional: use this if you want to process client events
+
+				Client hosebirdClient = builder.build();
+				return hosebirdClient;
+				
+	}
+	
+	//Criando um kafkaProducer
+	public KafkaProducer<String, String> createKafkaProducer() {
+		// TODO Auto-generated method stub
+		
+		  String bootstrapServers = "127.0.0.1:9092";
+		
+		  
+		 // 1 - Fazer as propriedades do producer
+		    Properties properties = new Properties();
+		   //Configurando as propriedades do topico utilizando o ProducerConfig do pacote clients do kafka    
+		    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); //endereço do broker kafka
+		    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());//Informando que o tipo de serializador é String ou seja vou mandar texto para o kafka
+		    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());//Informando que o tipo de serializador é String ou seja vou mandar texto para o kafka
+		  
+		    //Proprieddes para um producer seguro
+		    properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		    properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+		    properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+		    properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");// se for kafka>1.0 então 5 se for menor 1
+		    
+		    // Configuração para dar alto rendimento do producer
+		    properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+		    properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");//atraso para receber as mensagens quanto mais atrasar melhor o fator de compressão(fazer um micro-batch) atraso sempre em milesegundos
+		    properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));//32KB tamanho do batch
+		    
+		 // 2 - Criar o producer
+		    //Crio um produtor com chave e valor sendo String e passo as configurações acima para o mesmo
+		    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+		return producer ;
+	}
+}
+
+
